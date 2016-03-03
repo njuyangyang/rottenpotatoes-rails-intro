d60d28697a3b116c91aa003fd9cb010cd092318b
njuyangyang <njuyangyang@tamu.edu>
2016-03-03 16:44:53 -0600

    hw 2 part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,16 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    case params[:sort]
+    when 'title'
+      @movies=Movie.order('title ASC')
+      @title_style='hilite'
+    when 'release_date'
+      @movies=Movie.order('release_date ASC')
+      @release_style='hilite'
+    else
+      @movies=Movie.all
+    end    
   end
 
   def new
